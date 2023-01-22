0454a4dafc9981f5f2c5408fcbd66529682b8bd5
Rajesh Budhathoki <rajoyish@gmail.com>
2023-01-22 18:36:51 +0545

    Work on creating a post and image upload with spatie laravel media library plugin

--- app/Filament/Resources/PostResource.php
+++ app/Filament/Resources/PostResource.php
@@ -4,10 +4,21 @@ namespace App\Filament\Resources;
 
 use App\Filament\Resources\PostResource\Pages;
 use App\Models\Post;
+use Closure;
+use Filament\Forms\Components\Card;
+use Filament\Forms\Components\RichEditor;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\SpatieMediaLibraryFileUpload;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Toggle;
 use Filament\Resources\Form;
 use Filament\Resources\Resource;
 use Filament\Resources\Table;
 use Filament\Tables;
+use Filament\Tables\Columns\IconColumn;
+use Filament\Tables\Columns\SpatieMediaLibraryImageColumn;
+use Filament\Tables\Columns\TextColumn;
+use Illuminate\Support\Str;
 
 class PostResource extends Resource
 {
@@ -19,7 +30,27 @@ class PostResource extends Resource
     {
         return $form
             ->schema([
-                //
+                Card::make()->schema([
+                    Select::make('category_id')
+                        ->relationship('category', 'name'),
+
+                    TextInput::make('title')
+                        ->reactive()
+                        ->afterStateUpdated(function (Closure $set, $state) {
+                            $set('slug', Str::slug($state));
+                        })->required(),
+                    TextInput::make('slug')->required(),
+
+                    SpatieMediaLibraryFileUpload::make('thumbnail')
+                        ->collection('posts')
+                        ->multiple()
+                        ->enableReordering(),
+
+                    RichEditor::make('content'),
+
+                    Toggle::make('is_published'),
+
+                ]),
             ]);
     }
 
@@ -27,7 +58,13 @@ class PostResource extends Resource
     {
         return $table
             ->columns([
-                //
+                TextColumn::make('id')->sortable(),
+                TextColumn::make('title')->limit(50)->sortable(),
+                TextColumn::make('slug')->limit(50),
+                IconColumn::make('is_published')
+                    ->boolean(),
+                SpatieMediaLibraryImageColumn::make('thumbnail')
+                    ->collection('posts'),
             ])
             ->filters([
                 //
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,12 +6,18 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
 
-class Post extends Model
+class Post extends Model implements HasMedia
 {
-    use HasFactory;
+    use HasFactory, InteractsWithMedia;
 
     protected $guarded = [];
+
+    protected $casts = [
+        'is_published' => 'boolean',
+    ];
 
     public function category(): BelongsTo
     {
